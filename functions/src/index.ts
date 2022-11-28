0363dc2d0c5151eea91722813a50d97b8ab024c0
kromate <akpananthony33@gmail.com>
2022-11-28 11:10:16 +0100

    feat: done with verification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,36 +1,50 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import {IncomingWebhook} from '@slack/webhook';
+// .IncomingWebhook;
 admin.initializeApp();
 import sgMail = require('@sendgrid/mail');
 
-
 exports.userFirstTimeProfileUpdate = functions
-	.runWith({secrets: ['SENDGRID_KEY']})
+	.runWith({secrets: ['SENDGRID_KEY', 'SLACK_WEBHOOK_URL']})
 	.firestore.document('users/{userId}')
 	.onCreate(async (snap: any) => {
+		const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+		const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL as string);
 		const oldValues = snap.data();
-		const user_name = oldValues.first_name ? oldValues.first_name : oldValues.email;
+		const user_name = oldValues.first_name ?
+			oldValues.first_name :
+			oldValues.email;
 		const SENDGRID_KEY = process.env.SENDGRID_KEY;
-    sgMail.setApiKey(SENDGRID_KEY as string);
+		sgMail.setApiKey(SENDGRID_KEY as string);
 
 		const msg = {
 			to: oldValues.email,
-      from: 'Taaskly <anthony@taaskly.xyz>',
-      subject: 'Welcome to Taaskly !!!',
-      text: `Hello ${user_name}, Welcome to Taaskly !!! <br> <br> At Taaskly we are trying to create new ways for you get your tasks done and also earn money with ease. <br> <br> We are currently in beta and we would love to hear your feedback. <br> <br> Please feel free to reach out to us at <a href="mailto:anthony@taaskly.xyz"> anthony@taaskly.xyz </a> or <a href="https://wa.me/+2348115222468?" target="_blank" rel="noopener noreferrer">+2348115222468</a> <br> <br> Thanks, <br> <br>  Taaskly Team`,
-      html: `Hello ${user_name}, Welcome to Taaskly !!! <br> <br> At Taaskly we are trying to create new ways for you get your tasks done and also earn money with ease. <br> <br> We are currently in beta and we would love to hear your feedback. <br> <br> Please feel free to reach out to us at <a href="mailto:anthony@taaskly.xyz"> anthony@taaskly.xyz </a> or <a href="https://wa.me/+2348115222468?" target="_blank" rel="noopener noreferrer">+2348115222468</a> <br> <br> Thanks, <br> <br>  Taaskly Team`,
+			from: 'Taaskly <anthony@taaskly.xyz>',
+			subject: 'Welcome to Taaskly !!!',
+			text: `Hello ${user_name}, Welcome to Taaskly !!! <br> <br> At Taaskly we are trying to create new ways for you get your tasks done and also earn money with ease. <br> <br> We are currently in beta and we would love to hear your feedback. <br> <br> Please feel free to reach out to us at <a href="mailto:anthony@taaskly.xyz"> anthony@taaskly.xyz </a> or <a href="https://wa.me/+2348115222468?" target="_blank" rel="noopener noreferrer">+2348115222468</a> <br> <br> Thanks, <br> <br>  Taaskly Team`,
+			html: `Hello ${user_name}, Welcome to Taaskly !!! <br> <br> At Taaskly we are trying to create new ways for you get your tasks done and also earn money with ease. <br> <br> We are currently in beta and we would love to hear your feedback. <br> <br> Please feel free to reach out to us at <a href="mailto:anthony@taaskly.xyz"> anthony@taaskly.xyz </a> or <a href="https://wa.me/+2348115222468?" target="_blank" rel="noopener noreferrer">+2348115222468</a> <br> <br> Thanks, <br> <br>  Taaskly Team`,
 		};
 
-    await admin.auth().setCustomUserClaims(oldValues.id, {hasUpdatedProfile: true});
-      return sgMail.send(msg);
+		await admin
+			.auth()
+			.setCustomUserClaims(oldValues.id, {hasUpdatedProfile: true});
+		await webhook.send({
+			icon_emoji: ':male-police-officer:',
+			text: `Hi admins, ${user_name} with the ID of ${oldValues.id} just signed up`,
+		});
+		return sgMail.send(msg);
 	});
 
 exports.updateVerificationLevel = functions
 	.region('us-central1')
-	.https.onCall((data: any, context: any) => {
+	.https.onCall(async (data: any, context: any) => {
 		const level = data.level;
 		const uid = context?.auth?.uid;
-
+		const name = context?.auth?.token?.name;
+		// const email = context?.auth?.token?.email;
+	const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+		const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL as string);
 		if (!(typeof level === 'number') || level > 2) {
 			throw new functions.https.HttpsError(
 				'invalid-argument',
@@ -45,7 +59,10 @@ exports.updateVerificationLevel = functions
 				'The function must be called ' + 'while authenticated.'
 			);
 		}
-
+	await webhook.send({
+			icon_emoji: ':male-police-officer:',
+			text: `Hi admins,  user ${name} with ID of ${uid} is requesting to be verified`,
+		});
 		return admin
 			.firestore()
 			.collection('users')
@@ -55,5 +72,3 @@ exports.updateVerificationLevel = functions
 				return {level: `Verification level updated to ${level} successfully`};
 			});
 	});
-
-
