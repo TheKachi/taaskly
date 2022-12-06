48bec97b7c9a88b9544dab271406833baf62b3bb
kromate <akpananthony33@gmail.com>
2022-12-06 14:09:55 +0100

    maps

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,12 +1,11 @@
 
 <template>
-	<main ref="mapDiv" class="w-full h-full rounded-lg z-20 bg-slate-600 relative">
-		<div class="absolute bg-primary p-4 bottom-4 right-4 rounded-xl">
-			<button class="bg-white btn">
-				Create a Task
-			</button>
-		</div>
-	</main>
+	<div ref="mapDiv" class="w-full h-full rounded-lg z-20 bg-slate-600 relative" />
+	<div class="absolute bg-primary p-4 bottom-4 right-4 rounded-xl z-50 border-2 border-black">
+		<button class="bg-white btn px-10">
+			Create a Task
+		</button>
+	</div>
 </template>
 
 <script lang="ts" setup>
