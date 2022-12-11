c7c8f60733a25364cd2c84ea5709ddf578e0dc0f
kromate <akpananthony33@gmail.com>
2022-12-11 10:39:03 +0100

    feat: update

---
+++ src/components/tasks/Card.vue
@@ -0,0 +1,30 @@
+<template>
+	<article v-for="n in 10" :key="n" class="flex p-4  border-t border-secondary shadow cursor-pointer" @click="$router.push(`/tasks/${n}`)">
+		<div class="flex-shrink-0">
+			<Avatar />
+		</div>
+		<div class="ml-4 flex flex-col">
+			<div class="text-lg font-bold">
+				<a href="#" class="text-primary hover:text-teal-600">@username</a>
+			</div>
+			<div class="text-xs text-gray-500">
+				5m
+			</div>
+			<div class="text-[15px] text-gray-700">
+				I need someone to help me get a package from new hall Jumia pickup center and deliver it to me
+			</div>
+			<div class="text-[15px] text-gray-700 mt-3 flex gap-4">
+				<span class="btn-primary">Accept</span>
+				<span class="btn-secondary" @click.stop="useCoreModal().openSocialShare()">share</span>
+			</div>
+		</div>
+	</article>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,26 +1,7 @@
 
 <template>
 	<main class="flex-col flex items-stretch overflow-y-auto pt-16">
-		<article v-for="n in 10" :key="n" class="flex p-4  border-t border-secondary shadow cursor-pointer" @click="$router.push(`/tasks/${n}`)">
-			<div class="flex-shrink-0">
-				<Avatar />
-			</div>
-			<div class="ml-4 flex flex-col">
-				<div class="text-lg font-bold">
-					<a href="#" class="text-primary hover:text-teal-600">@username</a>
-				</div>
-				<div class="text-xs text-gray-500">
-					5m
-				</div>
-				<div class="text-[15px] text-gray-700">
-					I need someone to help me get a package from new hall Jumia pickup center and deliver it to me
-				</div>
-				<div class="text-[15px] text-gray-700 mt-3 flex gap-4">
-					<span class="btn-primary">Accept</span>
-					<span class="btn-secondary" @click.stop="useCoreModal().openSocialShare()">share</span>
-				</div>
-			</div>
-		</article>
+		<LazyTasksCard />
 	</main>
 </template>
 
@@ -35,10 +16,4 @@ definePageMeta({
 
 <style scoped lang="scss">
 
-.hi{
-	height:calc(100% );
-	@media (max-width: 640px){
-		height:calc(100% - 70px);
-	}
-}
 </style>
