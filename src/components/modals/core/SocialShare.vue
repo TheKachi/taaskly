32985d8f31977c4c4a4e2695cf455d693577e896
kromate <akpananthony33@gmail.com>
2022-12-11 14:19:49 +0100

    feat: adding icons

--- src/components/modals/core/SocialShare.vue
+++ src/components/modals/core/SocialShare.vue
@@ -4,17 +4,23 @@
 		title="Share Task"
 	>
 		<section class="flex flex-col gap-4">
-			<button class="btn font-semibold bg-[#075e54] text-white w-full">
-				<icon class="w-6 mr-2" name="clipboard" />
-				Copy to Clipboard
+			<button class="btn font-semibold bg-slate-800 text-white w-full">
+				<span class="w-64 flex items-center">
+					<icon class="w-6 mr-2" name="clipboard" />
+					Copy to Clipboard
+				</span>
 			</button>
 			<button class="btn font-semibold bg-[#075e54] text-white w-full">
-				<icon class="w-6 mr-2" name="whatsapp_outline" />
-				Whatsapp
+				<span class="w-64 flex items-center">
+					<icon class="w-6 mr-2" name="whatsapp_outline" />
+					Whatsapp
+				</span>
 			</button>
-			<button class="btn font-semibold bg-[#075e54] text-white w-full">
-				<icon class="w-6 mr-2" name="whatsapp_outline" />
-				Twitter
+			<button class="btn font-semibold bg-[#1DA1F2] text-white w-full">
+				<span class="w-64 flex items-center">
+					<icon class="w-6 mr-2" name="whatsapp_outline" />
+					Twitter
+				</span>
 			</button>
 		</section>
 	</Modal>
