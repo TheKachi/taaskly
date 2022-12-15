302af04437237161829b3c42ae0b6eec62d368d5
kromate <akpananthony33@gmail.com>
2022-12-15 11:36:40 +0100

    working-on-cards

---
+++ src/components/core/BadgeSmall.vue
@@ -0,0 +1,27 @@
+<template>
+	<span
+		v-if="name"
+		class="py-0.5 px-2 font-medium text-xs  rounded-3xl bg-primary text-white"
+	>
+		<slot>{{ name }}</slot>
+	</span>
+</template>
+
+<script setup lang='ts'>
+
+// const toLowerCase = (val) => {
+// 	return val.toLowerCase()
+// }
+// const toCapitalized = (data:string) => {
+// 	console.log(data)
+// const capitalized = data.charAt(0).toUpperCase() + data.slice(1).toLowerCase()
+//     return capitalized
+// }
+
+const props = defineProps<{
+  name:number | string
+}>()
+
+const selectPalette = ref<{color:string, bg:string}>()
+
+</script>
--- src/components/tasks/Card.vue
+++ src/components/tasks/Card.vue
@@ -1,21 +1,19 @@
 <template>
 	<article v-for="n in 10" :key="n" class="flex p-4  border-t border-secondary shadow cursor-pointer" @click="$router.push(`/tasks/${n}`)">
 		<div class="flex-shrink-0">
-			<Avatar />
+			<Avatar name="kromate" />
 		</div>
-		<div class="ml-4 flex flex-col">
+		<div class="ml-4 flex flex-col items-start w-full">
 			<div class="text-lg font-bold">
-				<a href="#" class="text-primary hover:text-teal-600">@username</a>
+				<a href="#" class="text-primary hover:text-teal-600">Kromate</a>
 			</div>
-			<div class="text-xs text-gray-500">
-				5m
-			</div>
-			<div class="text-[15px] text-gray-700">
+			<BadgeSmall name="Errand" class="!bg-black" />
+			<div class="text-[15px] text-gray-700 mt-2">
 				I need someone to help me get a package from new hall Jumia pickup center and deliver it to me
 			</div>
 			<div class="text-[15px] text-gray-700 mt-3 flex gap-4">
-				<span class="btn-primary">Accept</span>
-				<span class="btn-secondary" @click.stop="useCoreModal().openSocialShare()">share</span>
+				<span class="text-base text-primary font-bold">Accept</span>
+				<span class="text-base text-secondary font-bold" @click.stop="useCoreModal().openSocialShare()">share</span>
 			</div>
 		</div>
 	</article>
