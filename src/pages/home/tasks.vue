fc92e97aa3c96c5f5f804ccfa0ecbdf2e3002f79
kromate <akpananthony33@gmail.com>
2022-12-28 18:19:24 +0100

    feat: breaking downtask

---
+++ src/pages/home/tasks.vue
@@ -0,0 +1,14 @@
+<template>
+	<LazyTasksReferralCard />
+	<LazyTasksCard v-for="task in tasks" :id="task.id" :key="task.id" :task="task" />
+</template>
+
+<script setup lang="ts">
+import { useFetchHomeTasks } from '@/composables/tasks'
+const { fetchHomeTasks, loading, tasks } = useFetchHomeTasks()
+fetchHomeTasks()
+</script>
+
+<style scoped>
+
+</style>
