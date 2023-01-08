3782edae8f62530043decb7c7791fac1caa31c51
kromate <akpananthony33@gmail.com>
2023-01-08 12:44:58 +0100

    feat: update

---
+++ src/pages/service/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		Hello World from service
+	</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+	layout: 'shop-default',
+	middleware: ['is-authenticated', 'has-profile']
+})
+</script>
+
+<style scoped>
+
+</style>
