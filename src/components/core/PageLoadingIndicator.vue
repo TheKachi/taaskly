2952c9067c813f2be038c4e81148c3c7fba305d3
kromate <akpananthony33@gmail.com>
2023-01-13 11:35:08 +0100

    update

--- src/components/core/PageLoadingIndicator.vue
+++ src/components/core/PageLoadingIndicator.vue
@@ -1,13 +1,8 @@
 <template>
-	<div class="h-screen">
-		<div
-			v-if="loading"
-			class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500"
-		/>
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
-	</div>
+	<div
+		v-if="loading"
+		class="fixed left-0 top-0 h-1.5 w-full  bg-green-500 z-[9999]"
+	/>
 </template>
 <script setup lang="ts">
 const nuxtApp = useNuxtApp()
