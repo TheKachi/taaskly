e706ae5a0e11f47017e904310641e39a6b4c4520
classyrazy <classyrazy014@gmail.com>
2023-01-20 02:26:40 +0100

    Implemented Coming Soon Component

---
+++ src/components/core/ComingSoon.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<div
+			class="flex flex-col gap-4 justify-center"
+		>
+			<div>
+				<img
+					src="../../assets/images/main/construction-img.svg"
+					class="w-full h-full object-contain max-w-[400px] mx-auto"
+					alt=" Coming Soon illustration"
+				>
+			</div>
+			<h2
+				class="text-2xl font-semibold text-center"
+			>
+				{{ title }}
+			</h2>
+			<p class="text-xl font-medium text-center max-w-[500px] mx-auto">
+				{{ description }}
+			</p>
+			<button class="btn bg-dark w-[60%] mx-auto mt-3 text-light">
+				Join Waitlist
+			</button>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  description: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: 'Coming Soon',
+  description: 'Coming Soon description'
+})
+</script>
+
+<style scoped>
+/* .align-main-center{
+
+} */
+</style>
--- src/pages/main/shops/index.vue
+++ src/pages/main/shops/index.vue
@@ -1,10 +1,15 @@
 <template>
-	<main class="p-4 flex gap-3 flex-wrap center">
-		<div v-for="n in 10" :key="n" class="card">
+	<main class="p-4 flex gap-3 flex-wrap center min-h-[80vh]">
+		<!-- <div v-for="n in 10" :key="n" class="card">
 			<h1 class="text-dark text-5xl">
 				Shoes
 			</h1>
-		</div>
+		</div> -->
+		<LazyComingSoon
+			class="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]"
+			title="Shops Coming Soon"
+			description="Using an online shop to get access to various tools to help you grow your business."
+		/>
 	</main>
 </template>
 
@@ -16,23 +21,23 @@ definePageMeta({
 </script>
 
 <style scoped>
-.card{
+.card {
 	@apply border border-dark;
-	    height: 200px;
-    width: 100%;
+	height: 200px;
+	width: 100%;
 	max-width: 250px;
-    border-radius: 8px;
-    box-shadow: 3px 6px 10px 0 rgb(0 102 245 / 7%);
-    cursor: pointer;
-    transition: border .3s ease,transform .3s ease;
-    display: flex;
-    align-items: center;
-    padding: 24px 20px 32px 24px;
-    background-color: #fff;
-    position: relative;
-    margin-right: 10px;
-	    background-color: #e6f9ff;
-    box-shadow: 3px 6px 10px 0 rgb(244 201 210 / 17%);
+	border-radius: 8px;
+	box-shadow: 3px 6px 10px 0 rgb(0 102 245 / 7%);
+	cursor: pointer;
+	transition: border .3s ease, transform .3s ease;
+	display: flex;
+	align-items: center;
+	padding: 24px 20px 32px 24px;
+	background-color: #fff;
+	position: relative;
+	margin-right: 10px;
+	background-color: #e6f9ff;
+	box-shadow: 3px 6px 10px 0 rgb(244 201 210 / 17%);
 
 }
 </style>
