49e88afde32b6f963b37eed4a0b2b7310d658bbc
kromate <akpananthony33@gmail.com>
2023-01-23 16:28:56 +0100

    -

--- src/components/topbar/main/HomeNavBar.vue
+++ src/components/topbar/main/HomeNavBar.vue
@@ -11,55 +11,10 @@
 						src="/logo-text.svg"
 						alt="logo"
 						class="h-8"
+						width="130px"
+						height="32px"
 					>
 				</a>
-
-				<!-- <div class="hidden gap-7 ml-20  lg:flex">
-					<a href="/#onboard" class="nav-item">
-						About
-					</a>
-					<span to="#" class="nav-item option relative flex gap-2 items-center cursor-pointer">
-						Schools <Icon name="down" class="w-4 " />
-						<div class="mobile-card !pt-48">
-							<div class=" transite shadow-card">
-								<div
-									class="flex flex-col items-start gap-3.5"
-								>
-									<a href="#">
-										School of Product
-									</a>
-									<a href="#">
-										School of Engineering
-									</a>
-									<a href="#">
-										School of Data
-									</a>
-								</div>
-							</div>
-						</div>
-					</span>
-					<nuxt-link to="#" class="nav-item">
-						Join the Community
-					</nuxt-link>
-
-					<span class="nav-item option relative flex gap-2 items-center cursor-pointer">
-						help <Icon name="down" class="w-4 " />
-						<div class="mobile-card">
-							<div class=" transite shadow-card">
-								<div
-									class="flex flex-col items-start gap-3.5"
-								>
-									<nuxt-link to="/faqs">
-										FAQs
-									</nuxt-link>
-									<a href="#footer">
-										Contact Us
-									</a>
-								</div>
-							</div>
-						</div>
-					</span>
-				</div> -->
 			</div>
 			<div class=" items-center justify-between gap-3 pc">
 				<nuxt-link to="/auth/login" class="btn-secondary">
@@ -70,18 +25,12 @@
 				</nuxt-link>
 			</div>
 
-			<!-- <icon
-				class="mobile w-6 cursor-pointer z-50"
-				:name="show ? 'close' : 'menu'"
-				@click="toggleMenu"
-			/> -->
-
 			<component :is="show ? close : menu" class="cursor-pointer mobile" @click="toggleMenu" />
 		</header>
 
 		<div
 			v-if="show"
-			class="fixed inset-0 bg-blue bg  h-screen pt-32 px-5 bg-white"
+			class="fixed inset-0 bg-light bg  h-screen pt-32 px-5 bg-white"
 		>
 			<nuxt-link to="/auth/login" class="btn-secondary mb-4">
 				Log In
@@ -89,32 +38,6 @@
 			<nuxt-link to="/auth/register" class="btn-primary">
 				Sign up
 			</nuxt-link>
-
-			<!-- <div class="grid grid-cols-2 mt-[45px] gap-4 gap-y-12">
-				<div class="flex flex-col gap-[30px]">
-					<h1 class="font-bold">
-						SCHOOLS
-					</h1>
-					<span class="font-medium text-sm">School of Product</span><span class="font-medium text-sm">School of Data</span><span class="font-medium text-sm">School of Systems Eng</span>
-				</div>
-				<div class="flex flex-col gap-[30px]">
-					<h1 class="font-bold">
-						COMPANY
-					</h1>
-
-					<span class="font-medium text-sm">About</span><nuxt-link to="/faqs" class="font-medium text-sm">
-						FAQs
-					</nuxt-link><span class="font-medium text-sm">Join the Community</span><span class="font-medium text-sm">Partnership</span>
-				</div>
-
-				<div class="flex flex-col gap-[30px]">
-					<h1 class="font-bold">
-						HELP
-					</h1>
-
-					<a class="font-medium text-sm" href="mailto:axiaafrica@gmail.com">axiaafrica@gmail.com</a><a class="font-medium text-sm" href="tel:+2349015558035">09015558035</a>
-				</div>
-			</div> -->
 		</div>
 	</nav>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -27,67 +27,16 @@
 // const { googleSignin, signOut } = useSignin()
 // const { isLoggedIn, user } = useUser()
 
-const badgeArray = ref([
-	{ name: 'Shoes', top: '15%', right: '20%' },
-	{ name: 'Cakes', top: '20%', right: '15%' },
-	{ name: 'Wigs', top: '25%', right: '10%' },
-	{ name: 'Deliveries', top: '30%', right: '5%' },
-	{ name: 'Errands', top: '35%', right: '0%' },
-	{ name: 'Tutorials', top: '40%', right: '5%' },
-	{ name: 'Capenter', top: '45%', right: '10%' },
-	{ name: 'Electrian', top: '50%', right: '15%' },
-	{ name: 'Painting', top: '55%', right: '10%' },
-	{ name: 'Home cleaning', top: '60%', right: '15%' },
-	{ name: 'Marketing', top: '65%', right: '20%' },
-	{ name: 'Design', top: '70%', right: '25%' },
-	{ name: 'Programming', top: '75%', right: '20%' },
-	{ name: 'design', top: '80%', right: '15%' }
-	// { name: 'Photography', top: '20%', right: '15%' },
-	// { name: 'Printing', top: '20%', right: '15%' },
-	// { name: 'Project Research', top: '20%', right: '15%' },
-	// { name: 'Hair Stylist', top: '20%', right: '15%' },
-	// { name: 'Barber', top: '20%', right: '15%' },
-	// { name: 'Dry cleaner', top: '20%', right: '15%' },
-	// { name: 'Plumber', top: '20%', right: '15%' },
-	// { name: 'Rice', top: '20%', right: '15%' },
-	// { name: 'Laptop', top: '20%', right: '15%' },
-	// { name: 'Power Bank', top: '20%', right: '15%' },
-	// { name: 'Chicken', top: '20%', right: '15%' },
-	// { name: 'Mobile Phone', top: '20%', right: '15%' }
-])
-
 definePageMeta({
 	layout: 'landing'
 })
 </script>
 
 <style scoped>
-a{
-	@apply underline hover:scale-105 cursor-pointer;
-}
 h1 {
         font-size: clamp(40px, 8.8vw, 58px);
         font-weight: 800;
 		line-height: clamp(40px, 9vw, 75px) ;
     }
-/* .grad{
-background: linear-gradient(65deg, #89216b , var(--red));
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-} */
-.grad {
-  background: -webkit-linear-gradient(245.29deg, #6DC1DC 13.32%, #A690FC 28.55%, #FC96BB 55.55%, #FFC397 84.12%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-    animation: hue 1.5s infinite linear;
-	 -webkit-text-stroke: 1px var(--primary);
-}
-@keyframes hue {
-  from {
-    -webkit-filter: hue-rotate(0deg);
-  }
-  to {
-    -webkit-filter: hue-rotate(360deg);
-  }
-}
+
 </style>
